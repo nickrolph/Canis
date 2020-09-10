4da4da8315163ba881f41848220044a3facad0c5
Nicholas <nickrolph@gmail.com>
2020-09-10 12:31:34 -0700

    added menu wireframe

--- canis/src/App.js
+++ canis/src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./tailwind.css";
+import HelloWorld from "./Components/HelloWorld";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <h1 className="font-bold text-2xl">This is the home page</h1>
+          </Route>
+          <Route path="/about">
+            <h1 className="font-bold text-2xl">This is the About page</h1>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
