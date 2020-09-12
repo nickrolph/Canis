ed4de0fb5933aec0e319174e10334c4a7ca01516
Nicholas <nickrolph@gmail.com>
2020-09-11 17:16:08 -0700

    removed unused file, added temp logo to header

--- canis/src/Components/Header.js
+++ canis/src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./Navigation";
+import logo from "../tempGoldenLogo.svg";
 
 function Header() {
   return (
     <header className="border-b p-3 flex justify-between items-center">
-      <span className="font-bold"> Golden Retriever </span>
+      <img src={logo} alt="Golden Retriever Temp Logo" />
+      <span className="font-bold text-xl"> Golden Retriever </span>
       <Navigation />
     </header>
   );
