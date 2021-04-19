fc667ac34d96f60ec982e47f6238fb5b2699ed14
Nicholas <nickrolph@gmail.com>
2021-04-19 14:23:49 -0700

    added first tests

---
+++ jenkins/scripts/test.sh
@@ -0,0 +1,9 @@
+echo 'This command is for a devDependency'
+set -x
+#npm install --save-dev cross-env
+set +x
+
+echo 'the following "npm" command tests that our application is rendering'
+echo 'this comman invokes: runner Jest (https://facebook.github.io/jest/)'
+set -x
+npm test
