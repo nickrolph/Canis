63e10bb14e1db0dfc943fe616ab1125bdbe57cc0
Nicholas <nickrolph@gmail.com>
2021-04-19 14:36:45 -0700

    fixed accessibility of file

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env sh
+
 echo 'This command is for a devDependency'
 set -x
 #npm install --save-dev cross-env
 set +x
 
 echo 'the following "npm" command tests that our application is rendering'
-echo 'this comman invokes: runner Jest (https://facebook.github.io/jest/)'
+echo 'this command invokes: runner Jest (https://facebook.github.io/jest/)'
 set -x
 npm test
